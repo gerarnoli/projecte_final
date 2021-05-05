43696a256c753aff9263f61e68913f5862ba2083
Gerry <a20gerarnoli@inspedralbes.cat>
2021-05-05 12:56:52 +0200

    datos api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
-
+ 
+const url = "https://api.covid19tracking.narrativa.com/api/2021-05-05/country/spain";
+ 
 function App() {
+ 
+  const [userData, setUserData] = useState({});
+ 
+  useEffect( () => {
+    getUrlWithfetch();
+  }, []);
+ 
+  const getUrlWithfetch = async () => {
+    const response = await axios.get(url);    
+    console.log(response.data);
+    setUserData(response.data.dates["2021-05-05"].countries.Spain.regions);    
+    //console.log(response.data.dates["2021-05-05"].countries.Spain.regions);
+    console.log(response.data.dates["2021-05-05"].countries.Spain.regions);
+  };
+ 
+  const listaComunidades = userData.map((c) => <div key={c.id}>{c.id}</div>);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <header className="App-header">
+            <h2>Datos Kovid  {userData.updated_at}</h2>
+          </header>
+ 
+          <body className="App-body">
+          <h4> pepe {userData[5].id}</h4>
+        <div>
+          {listaComunidades}
+        </div>
+         
+          </body>
+ 
+     </div>     
   );
+ 
+ 
 }
-
+ 
+ 
 export default App;
