e3417abff61035dc9a16a255cd982b1e598a303d
gerarnoli <a20gerarnoli@inspedralbes.cat>
2021-05-11 11:40:21 +0200

    covidlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
-import CovidList from "./omponent/CovidList";
+import CovidList from "./component/CovidList";
 
 import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
