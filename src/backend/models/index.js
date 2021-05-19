ceb1ca887b750a054896a420b35d0a627352124d
Pedro Clase <a16pednieper@inspedralbes.cat>
2021-05-19 12:25:55 +0200

    Eliminar Codigo Sucio
    Ee

--- src/backend/models/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.set('debug', true);
-
-mongoose.connect('mongodb://localhost:27017/vacuna');
-
-mongoose.Promise = Promise;
-
-module.exports.Ccaa = require("./ccaa");
