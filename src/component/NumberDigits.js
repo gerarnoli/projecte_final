7b1325268fb2fff5aea7554c20aa7d62c2f958b2
gerarnan <gerarnan@pm.me>
2021-05-22 21:03:52 +0200

    canvis

--- src/component/NumberDigits.js
+++
@@ -1,8 +0,0 @@
-import { useLocale } from 'use-locale'
-
-export const toDigit = ({ locale, number }) => new Intl.NumberFormat(locale).format(number)
-
-export default function NumberDigits ({ children }) {
-  const { locale } = useLocale()
-  return toDigit({ locale, number: children })
-}
--- src/paginas/index.js
+++ src/paginas/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import CovidList from "../component/CovidList"; // Import de la API COVID
 
 const Home = () => {
     return (
         <div className="taula">
-            <CovidList/>
+            
         </div>
     )
 }
